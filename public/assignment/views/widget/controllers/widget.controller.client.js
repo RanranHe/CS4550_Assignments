30b132f7e8e0d813169067a1f5859b1ce5dafed8
cangxuexiao <cangxuexiao@gmail.com>
2017-06-06 08:37:49 +0800

    Heroku Error?

--- public/assignment/views/widget/controllers/widget.controller.client.js
+++ public/assignment/views/widget/controllers/widget.controller.client.js
@@ -6,8 +6,7 @@
         .module("WebAppMaker")
         .controller("WidgetListController", WidgetListController)
         .controller("WidgetChooserController", WidgetChooserController)
-        .controller("EditWidgetController", EditWidgetController)
-        .controller("FlickrImageSearchController", FlickrImageSearchController);
+        .controller("EditWidgetController", EditWidgetController);
 
     function WidgetListController($sce, $routeParams, WidgetService) {
         var model = this;
@@ -129,59 +128,6 @@
                 .updateWidget(model.widgetId, model.widget)
                 .then(function () {
                     $location.url('/user/' + model.userId + '/website/' + model.websiteId + '/page/' + model.pageId + /widget/);
-                });
-        }
-    }
-
-    function FlickrImageSearchController($location, $routeParams, WidgetService, FlickrService) {
-        var model = this;
-
-        model.userId = $routeParams['uid'];
-        model.websiteId = $routeParams['wid'];
-        model.pageId = $routeParams['pid'];
-        model.widgetId = $routeParams['wgid'];
-
-        model.searchPhotos = searchPhotos;
-        model.selectPhoto = selectPhoto;
-
-        function init() {
-            WidgetService
-                .findWidgetById(model.websiteId)
-                .then(function (widget) {
-                    model.widget = widget;
-                });
-        }
-
-        init();
-
-        function searchPhotos(searchTerm) {
-            FlickrService
-                .searchPhotos(searchTerm)
-                .then(function (response) {
-                    data = response.data.replace("jsonFlickrApi(", "");
-                    data = data.substring(0, data.length - 1);
-                    data = JSON.parse(data);
-                    model.photos = data.photos;
-                });
-        }
-
-        function selectPhoto(photo) {
-            var url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server;
-            url += "/" + photo.id + "_" + photo.secret + "_b.jpg";
-            widget = {
-                '_id': model.widgetId,
-                'name': '',
-                'widgetType': 'IMAGE',
-                'pageId': model.pageId,
-                'width': '',
-                'url': url,
-                'text': ''
-            };
-
-            WidgetService
-                .updateWidget(model.widgetId, widget)
-                .then(function () {
-                    $location.url('/user/' + model.userId + '/website/' + model.websiteId + '/page/' + model.pageId + '/widget/' + model.widgetId);
                 });
         }
     }

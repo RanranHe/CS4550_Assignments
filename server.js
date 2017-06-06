cd6dd28194d9f2a5dcb9073c3476a17053da0a50
cangxuexiao <cangxuexiao@gmail.com>
2017-06-06 11:07:49 +0800

    Heroku Error? Try to fix again

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-var app = require('./express');
+// var app = require('./express');
+//
+var express = require('express');
+var app = express();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencooded({ extended: true }));
 
-app.use(app.express.static(__dirname + '/public'));
+
+//
+//
+//
+// app.use(app.express.static(__dirname + '/public'));
+//
+// require ("./test/app.js")(app);
+// require('./assignment/app')(app);
+//
+// app.listen(process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 require('./assignment/app')(app);
 
-app.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port);

4ae1460806a0d0c0de9937db7ecbf2b56e686694
cangxuexiao <cangxuexiao@gmail.com>
2017-06-06 10:08:27 +0800

    Heroku Error? Try to fix again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('./express');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(app.express.static(__dirname + '/public'));
 

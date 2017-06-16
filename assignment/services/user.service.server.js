df259926f7708b92b384432065a9315435ca7eac
cangxuexiao <cangxuexiao@gmail.com>
2017-06-16 18:32:29 +0800

    Try to fix heroku facebook login

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -30,10 +30,8 @@ module.exports = function (app, models) {
     app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
-            // successRedirect: '/assignment/index.html#!/profile',
-            // failureRedirect: '/#!/login'
-            successRedirect: '/#/user',
-            failureRedirect: '/#/login'
+            successRedirect: '/assignment/index.html#!/profile',
+            failureRedirect: '/#!/login'
         }));
 
     app.get("/api/user/:userId", findUserById);
